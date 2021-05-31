0a3b0b41a90704d183501658451ccc82f6051fda
YuriiZu <jwestasf@gmail.com>
2021-05-31 14:53:58 +0000

    html

--- js/index.js
+++ js/index.js
@@ -31,8 +31,6 @@ const newsJson = `[
         "description": "Dates and Location Are Approved",
         "alt": "news-four"
     }
-
-    }
 ]`;
 
 function renderNews(news) {
@@ -53,3 +51,66 @@ function renderNews(news) {
 }
 
 renderNews(JSON.parse(newsJson));
+
+const lineUpJson = `[
+  {
+    "id": "1",
+    "title": "Tomas Eck",
+    "image": "tomas.webp",
+    "description": "His first tracks, "Beast" and "Silence" were released in 2006 on the Berlin label
+    Auter Records. In 2 years, Tomas became a constant favorite in today's club scene.",
+    "date": "5 June | West Stage"
+},
+
+{ 
+    "id": "2",
+    "title": "DJ Delilah",
+    "image": "img/dj.webp",
+    "description": "Her commitment and deep musical connection is the key to her status one of the best
+    techno DJs for the last 10 years. DJ Delilah stands as one of the American minimal techno pioneers.",
+    "date": "6 June | East Stage"
+},
+
+{
+    "id": "3",
+    "title": "Jayden Taylor",
+    "image": "img/jayden.webp",
+    "description": "Jayden combines his unique sound with a maximum of other genres.
+    He shows his audiences how exciting techno can be and experiments a lot.",
+    "date": "7 June | Main Stage"
+},
+
+{
+    "id": "4",
+    "title": "Mary Don",
+    "image": "img/mary.webp",
+    "description": "Mary Don is one of the most in-demand names in electronic dance music in 2020.
+    She takes cues from techno, funk, disco, and hiphop.",
+    "date": "9 June | East Stage"
+}
+
+]` ;
+
+function renderLineUp(lineUp) {
+  const lineUpContainer = document.querySelector(" .stars");
+  let html = "";
+  for (const star of lineUp) {
+    html += `<div class="card mb-3 bg-transparent border-0" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="img/${star.image}" class="rounded-circle" alt="${star.title}">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${star.title}</h5>
+          <p class="card-text">${star.date}</p>
+          <p class="card-text">${star.description}</p>
+        </div>
+      </div>
+    </div>
+  </div>`;
+  }
+  lineUpContainer.innerHTML = html;
+}
+
+renderLineUp(JSON.parse(lineUpJson));
